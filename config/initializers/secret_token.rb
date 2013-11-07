5f8752512b02c7a00021ad895ae4bbffe721829f
Tom Dyer <tdyer1@gmail.com>
2013-11-07 03:39:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SongsApp::Application.config.secret_key_base = 'be4d627731a793e55190fae9b4970a327b95649225cf364c8a687119b68a7eb7a21d651b101a4f05f49dc9424c3084ad324ec1d3c183d373eb0254b54e15a66d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SongsApp::Application.config.session_store :cookie_store, key: '_songs_app_session'
