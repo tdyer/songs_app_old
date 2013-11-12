661c7069f23d5897e9de518af2d928e1212e09cf
Tom Dyer <tdyer1@gmail.com>
2013-11-12 11:06:59 -0500

    Fix songs.rb syntax error.

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -2,33 +2,34 @@ $(document).ready(function(){
   // Need to use delegate here as the form with id='new_song' is 
   // *not* available when the page is loaded. This form is inserted into 
   // the page when clicking the "New Song" link.
-	$('#new_song_form_container').delegate('#new_song',"submit", function(event){
-		event.preventDefault();
-		var formData = {song: {name: '', duration: '', price: ''}};
-		formData.song.name = $('#song_name').val();
-		formData.song.duration = $('#song_duration').val();
-		formData.song.price = $('#song_price').val();
-		$.ajax({
-			url: '/songs',
-			type: 'POST',
-			dataType: 'script',
-			data: formData,
-			success: function(data){
-				console.log(data);
-				$('#new_song').remove();
-			}
-		});
-	});
+  $('#new_song_form_container').delegate('#new_song',"submit", function(event){
+  	event.preventDefault();
+  	var formData = {song: {name: '', duration: '', price: ''}};
+  	formData.song.name = $('#song_name').val();
+  	formData.song.duration = $('#song_duration').val();
+  	formData.song.price = $('#song_price').val();
+  	$.ajax({
+  		url: '/songs',
+  		type: 'POST',
+  		dataType: 'script',
+  		data: formData,
+  		success: function(data){
+  			console.log(data);
+  			$('#new_song').remove();
+  		}
+  	})
+  });
 
-	$('#new_link').click(function(event){
-	// var url = $('#new_link').href
-	event.preventDefault();
-	$.ajax({
-		url: '/songs/new',
-		dataType: 'script',
-		success: function(data){
-			console.log(data);
-		}
-	})
+  $('#new_link').click(function(event){
+	 event.preventDefault();
+	 $.ajax({
+	   url: '/songs/new',
+	   dataType: 'script',
+	 	 success: function(data){
+	 		event.preventDefault();
+	 		console.log(data);
+	 	}
+	 });
+  });
 });
 
